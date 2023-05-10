45ae587bf47ff42e445fc6afe43651fb809d69f4
Sathish Panthagani <sathish39893@gmail.com>
2023-05-10 11:54:33 +0530

    feat(userlist): add user listcomponent

--- src/app/users/layout.tsx
+++ src/app/users/layout.tsx
@@ -1,10 +1,17 @@
+import getUsers from '@/actions/getUsers';
+import UserList from '@/components/UserList';
 import Sidebar from '@/components/sidebar/Sidebar';
 
 const UsersLayout = async ({ children }: { children: React.ReactNode }) => {
+  const users = await getUsers();
+
   return (
     //@ts-expect-error Server Component
     <Sidebar>
-      <div className="h-full">{children}</div>
+      <div className="h-full">
+        <UserList items={users} />
+        {children}
+      </div>
     </Sidebar>
   );
 };
